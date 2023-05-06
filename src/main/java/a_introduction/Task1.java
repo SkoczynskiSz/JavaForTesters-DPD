04e976862690d97bfc878c36860ba413de72215f
SkoczynskiSz <szymon.skoczynski@dpdgroup.com>
2023-05-06 13:53:49 +0200

    Pierwszy komit

--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -1,8 +1,30 @@
 package a_introduction;
 
 public class Task1 {
+
+ /*   int a = 11;
+
+    char b = 'b';
+
+    double c = 0.12345678;
+
+    long d = -1500;
+*/
+    static boolean x = false;
+
     public static void main(String[] args) {
 
-set
+        int a = 11;
+
+        char b = 'b';
+        double c = 0.12345678;
+        long d = -1500;
+        boolean e = x;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
     }
+
 }
